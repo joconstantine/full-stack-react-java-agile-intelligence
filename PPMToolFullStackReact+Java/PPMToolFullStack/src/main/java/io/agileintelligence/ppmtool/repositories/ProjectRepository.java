8f25c1c6ae1c849fbf040bb0e4701079be8b33f4
joconstantine <joconstantine@users.noreply.github.com>
2021-06-07 13:59:16 +0800

    07/06/2021 13:59

---
+++ PPMToolFullStackReact+Java/PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.agileintelligence.ppmtool.repositories;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    public Project findByProjectIdentifier(String projectIdentifier);
+}
